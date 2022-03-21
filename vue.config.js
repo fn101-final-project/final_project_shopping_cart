18b0d5576f708cb835901e8f5941823d0df95238
April Hung <huotsai2fi@gmail.com>
2022-03-21 10:34:37 +0800

    adjust repo name

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,6 @@ module.exports = defineConfig({
   },
   publicPath:
     process.env.NODE_ENV === 'production'
-      ? '/final_project_shopping_cart_backend/'
+      ? '/final_project_shopping_cart/'
       : '/',
 });
