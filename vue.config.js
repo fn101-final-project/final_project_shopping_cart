5391a586dd3bf74717a78c085c089b741a8f2cba
April Hung <huotsai2fi@gmail.com>
2022-03-25 18:59:39 +0800

    feat: set env variable

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   pages: {
     index: {
-      // entry for the page
       entry: 'src/main.js',
       title: 'KOHOLA',
     },
   },
-  publicPath:
-    process.env.NODE_ENV === 'production'
-      ? '/final_project_shopping_cart/'
-      : '/',
+  publicPath: process.env.BASE_URL,
+  productionSourceMap: false,
 });
